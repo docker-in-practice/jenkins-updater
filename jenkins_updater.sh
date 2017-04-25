6096e054c1f35d6406b4a2db407fb9efc8c85589
Ian Miell <ian.miell@gmail.com>
2017-04-25 15:06:40 +0100

    50000

--- jenkins_updater.sh
+++ jenkins_updater.sh
@@ -6,5 +6,5 @@ then
 	docker stop jenkins
 	docker rename jenkins jenkins.bak.$(date +%Y%m%d%H%M)
 	cp -r /var/docker/mounts/jenkins_home /var/docker/mounts/jenkins_home.bak.$(date +%Y%m%d%H%M)
-	docker run -d --restart always -v /var/docker/mounts/jenkins_home:/var/jenkins_home --name jenkins -p 8080:8080 jenkins
+	docker run -d --restart always -v /var/docker/mounts/jenkins_home:/var/jenkins_home --name jenkins -p 8080:8080 -p 50000:50000 jenkins
 fi
