a068574356eaacf014505e245a9ca64ae352b33c
Ian Miell <ian.miell@gmail.com>
2017-04-25 10:52:13 +0100

    script

--- jenkins_updater.sh
+++ jenkins_updater.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+set -e
+if ! docker pull jenkins | grep up.to.date
+then
+	docker stop jenkins
+	docker rename jenkins jenkins.bak.$(date +\%Y\%m\%d\%H)
+	cp -r /var/docker/mounts/jenkins_home/ /var/docker/mounts/jenkins_home.bak.$(date +\%Y\%m\%d\%H)
+	docker run -d --restart always -v /var/docker/mounts/jenkins_home:/var/jenkins_home --name jenkins -p 8080:8080 jenkins)
+fi
