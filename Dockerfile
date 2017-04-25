e801d2734c8606d2de10e21bca322bcd6bf0c3db
Ian Miell <ian.miell@gmail.com>
2017-04-25 12:32:43 +0100

    tested

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM docker
 ADD jenkins_updater.sh /jenkins_updater.sh
 RUN chmod +x /jenkins_updater.sh
-CMD /jenkins_updater.sh
+ENTRYPOINT /jenkins_updater.sh
--- jenkins_updater.sh
+++ jenkins_updater.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 set -e
+set -x
 if ! docker pull jenkins | grep up.to.date
 then
 	docker stop jenkins
-	docker rename jenkins jenkins.bak.$(date +\%Y\%m\%d\%H)
-	cp -r /var/docker/mounts/jenkins_home/ /var/docker/mounts/jenkins_home.bak.$(date +\%Y\%m\%d\%H)
-	docker run -d --restart always -v /var/docker/mounts/jenkins_home:/var/jenkins_home --name jenkins -p 8080:8080 jenkins)
+	docker rename jenkins jenkins.bak.$(date +%Y%m%d%H%M)
+	cp -r /var/docker/mounts/jenkins_home /var/docker/mounts/jenkins_home.bak.$(date +%Y%m%d%H%M)
+	docker run -d --restart always -v /var/docker/mounts/jenkins_home:/var/jenkins_home --name jenkins -p 8080:8080 jenkins
 fi
