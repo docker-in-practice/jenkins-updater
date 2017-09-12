1496f9dae1e38cabf8a5dd96b788776828093ded
Ian Miell <ian.miell@gmail.com>
2017-09-12 09:06:41 +0100

    remove_old

--- jenkins_updater.sh
+++ jenkins_updater.sh
@@ -7,4 +7,5 @@ then
 	docker rename jenkins jenkins.bak.$(date +%Y%m%d%H%M)
 	cp -r /var/docker/mounts/jenkins_home /var/docker/mounts/jenkins_home.bak.$(date +%Y%m%d%H%M)
 	docker run -d --restart always -v /var/docker/mounts/jenkins_home:/var/jenkins_home --name jenkins -p 8080:8080 -p 50000:50000 jenkins
+	find /var/docker/mounts/jenkins_home.bak.* -maxdepth 0 -mindepth 0 -ctime +30 | xargs -n1 rm -rf
 fi
